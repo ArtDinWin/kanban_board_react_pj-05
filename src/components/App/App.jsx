99ac0f37f0ef022b4d0766c053eac0cc93b3602e
ArtDinWin <andsmirastrav@mail.ru>
2023-05-19 11:30:08 +0300

    fix layout and code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, createContext } from "react";
-// import { HashRouter as Router } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./../header/Header";
 import Main from "./../main/Main";
 import Footer from "./../footer/Footer";
@@ -22,7 +22,7 @@ function App() {
   }, [tasks, availableSave]);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <AppContext.Provider value={{ setAvailableSave }}>
         <div className={css.wrapper}>
           <Header tasks={tasks} setTasks={setTasks} />
